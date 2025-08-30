98c1943b631d85edeb82e631b4fa75b70dc2053f
m1nl <mateusz@nalewajski.pl>
2025-08-30 21:12:23 +0200

    define config and dts file for ebaz4205 board

---
+++ include/configs/zynq_ebaz4205.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * (C) Copyright 2013 - 2017 Xilinx.
+ *
+ * Configuration settings for the Xilinx Zynq CSE board.
+ * See zynq-common.h for Zynq common configs
+ */
+
+#ifndef __CONFIG_ZYNQ_CSE_H
+#define __CONFIG_ZYNQ_CSE_H
+
+#include <configs/zynq-common.h>
+
+#endif /* __CONFIG_ZYNQ_CSE_H */
