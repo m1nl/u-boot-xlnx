5f994aeba4108742058ede4d624681ea891f22fd
m1nl <mateusz@nalewajski.pl>
2025-09-22 23:44:56 +0200

    remove unneeded config options

--- include/configs/zynq_ebaz4205.h
+++ include/configs/zynq_ebaz4205.h
@@ -1,14 +1,8 @@
 /* SPDX-License-Identifier: GPL-2.0+ */
-/*
- * (C) Copyright 2013 - 2017 Xilinx.
- *
- * Configuration settings for the Xilinx Zynq CSE board.
- * See zynq-common.h for Zynq common configs
- */
 
-#ifndef __CONFIG_ZYNQ_CSE_H
-#define __CONFIG_ZYNQ_CSE_H
+#ifndef __CONFIG_ZYNQ_EBAZ4205_H
+#define __CONFIG_ZYNQ_EBAZ4205_H
 
 #include <configs/zynq-common.h>
 
-#endif /* __CONFIG_ZYNQ_CSE_H */
+#endif /* __CONFIG_ZYNQ_EBAZ4205_H */
