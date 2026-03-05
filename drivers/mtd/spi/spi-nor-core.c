m1nl/u-boot-xlnx

commit eb9e2e21cae5d6b555efedbd9ebf136d30a9d21d

mtd: spi-nor: Enable DDR mode functionality with ISSI flash
Enable the utilization of mt35xu512aba_fixups for ISSI octal
flash to operate in DDR mode.

Signed-off-by: Tejas Bhumkar <tejas.arvind.bhumkar@amd.com>
Signed-off-by: Venkatesh Yadav Abbarapu <venkatesh.abbarapu@amd.com>
Link: https://lore.kernel.org/all/12e89f942322f9e243f0e63bd3c10170e1fcd478.1710098033.git.tejas.arvind.bhumkar@amd.com/
State: waiting

drivers/mtd/spi/spi-nor-core.c
@@ -4121,7 +4121,7 @@ static struct spi_nor_fixups s28hx_t_fixups = {
 };
 #endif /* CONFIG_SPI_FLASH_S28HX_T */
 
-#ifdef CONFIG_SPI_FLASH_MT35XU
+#if defined(CONFIG_SPI_FLASH_MT35XU) || defined(CONFIG_SPI_FLASH_ISSI)
 static int spi_nor_micron_octal_dtr_enable(struct spi_nor *nor)
 {
 	struct spi_mem_op op;
@@ -6018,6 +6018,14 @@ void spi_nor_set_fixups(struct spi_nor *nor)
 	    !strcmp(nor->info->name, "mt35xu02g")) {
 		nor->fixups = &mt35xu512aba_fixups;
 	}
+#endif
+
+#if defined(CONFIG_SPI_FLASH_ISSI)
+       if (!strcmp(nor->info->name, "is25wx256") ||
+           !strcmp(nor->info->name, "is25lx512")) {
+               nor->fixups = &mt35xu512aba_fixups;
+               return;
+       }
 #endif
 
 #if CONFIG_IS_ENABLED(SPI_FLASH_MACRONIX)

drivers/mtd/spi/spi-nor-ids.c
@@ -242,9 +242,10 @@ const struct flash_info spi_nor_ids[] = {
 			SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ) },
 	{ INFO("is25wx256",  0x9d5b19, 0, 128 * 1024, 256,
 			SECT_4K | USE_FSR | SPI_NOR_OCTAL_READ | SPI_NOR_4B_OPCODES |
-			SPI_NOR_HAS_TB) },
+			SPI_NOR_HAS_TB | SPI_NOR_OCTAL_DTR_READ | SPI_NOR_OCTAL_DTR_PP) },
 	{ INFO("is25lx512",  0x9d5a1a, 0, 64 * 1024, 1024,
-			SECT_4K | USE_FSR | SPI_NOR_OCTAL_READ | SPI_NOR_4B_OPCODES | SPI_NOR_HAS_TB) },
+			SECT_4K | USE_FSR | SPI_NOR_OCTAL_READ | SPI_NOR_4B_OPCODES | SPI_NOR_HAS_TB |
+			SPI_NOR_OCTAL_DTR_READ | SPI_NOR_OCTAL_DTR_PP) },
 	{ INFO("is25lp01gg",  0x9d6021, 0, 64 * 1024, 2048,
 			SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ | SPI_NOR_HAS_TB) },
 #endif

commit d012a5e3789e7b8aed9823ddb41f034e36304af2

mtd: spi-nor: Fix coding style in ISSI DDR mode setup
Replace spaces with tabs for proper kernel coding style indentation
in the ISSI flash DDR mode fixup assignment section.

Fixes: eb9e2e21cae5 ("mtd: spi-nor: Enable DDR mode functionality with ISSI flash")
Signed-off-by: Michal Simek <michal.simek@amd.com>
Message-ID: <528c49371e25ea705f7da818cf80a8706695bcc1.1772610195.git.michal.simek@amd.com>

drivers/mtd/spi/spi-nor-core.c
@@ -6319,11 +6319,11 @@ void spi_nor_set_fixups(struct spi_nor *nor)
 #endif
 
 #if defined(CONFIG_SPI_FLASH_ISSI)
-       if (!strcmp(nor->info->name, "is25wx256") ||
-           !strcmp(nor->info->name, "is25lx512")) {
-               nor->fixups = &mt35xu512aba_fixups;
-               return;
-       }
+	if (!strcmp(nor->info->name, "is25wx256") ||
+	    !strcmp(nor->info->name, "is25lx512")) {
+		nor->fixups = &mt35xu512aba_fixups;
+		return;
+	}
 #endif
 
 #if defined(CONFIG_SPI_FLASH_GIGADEVICE)