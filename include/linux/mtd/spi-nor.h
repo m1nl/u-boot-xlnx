m1nl/u-boot-xlnx

commit 7f77ed0916e053b47842705156351e463efb0c65

mtd: spi-nor: Add support for flash reset
Add support for spi-nor flash reset via GPIO controller
by reading the reset-gpios property.

[Ported from Linux kernel commit
8f1ee9ef71d0 ("mtd: spi-nor: Add support for flash reset") ]

Signed-off-by: Venkatesh Yadav Abbarapu <venkatesh.abbarapu@amd.com>
Link: https://lore.kernel.org/u-boot/20241113091132.28272-1-venkatesh.abbarapu@amd.com/T/#u
State: waiting
[michals:
v2026.01 update
mtd: spi-nor: Fix build issue for stmark2_defconfig
]

include/linux/mtd/spi-nor.h
@@ -603,6 +603,7 @@ struct spi_nor {
 	u8			cmd_buf[SPI_NOR_MAX_CMD_SIZE];
 	enum spi_nor_cmd_ext	cmd_ext_type;
 	struct spi_nor_fixups	*fixups;
+	struct gpio_desc	*flash_gpio_reset;
 
 	int (*setup)(struct spi_nor *nor, const struct flash_info *info,
 		     const struct spi_nor_flash_parameter *params);

drivers/mtd/spi/spi-nor-core.c
@@ -12,6 +12,7 @@
 #include <display_options.h>
 #include <log.h>
 #include <watchdog.h>
+#include <asm-generic/gpio.h>
 #include <dm.h>
 #include <dm/device_compat.h>
 #include <dm/devres.h>
@@ -6010,6 +6011,39 @@ void spi_nor_set_fixups(struct spi_nor *nor)
 #endif /* SPI_FLASH_MACRONIX */
 }
 
+static int spi_nor_hw_reset(struct spi_nor *nor)
+{
+#if CONFIG_IS_ENABLED(DM_GPIO)
+	struct udevice *dev = nor->spi->dev;
+	int rc;
+
+	nor->flash_gpio_reset = devm_gpiod_get_optional(dev, "reset",
+							GPIOD_IS_OUT | GPIOD_ACTIVE_LOW);
+
+	if (nor->flash_gpio_reset) {
+		/*
+		 * Experimental delay values by looking at different flash device
+		 * vendors datasheets.
+		 */
+		udelay(5);
+
+		/* Toggle gpio to reset the flash device. */
+		rc = dm_gpio_set_value(nor->flash_gpio_reset, 1);
+		if (rc)
+			return rc;
+
+		udelay(150);
+
+		rc = dm_gpio_set_value(nor->flash_gpio_reset, 0);
+		if (rc)
+			return rc;
+
+		udelay(1200);
+	}
+#endif
+	return 0;
+}
+
 int spi_nor_scan(struct spi_nor *nor)
 {
 	struct spi_nor_flash_parameter params;
@@ -6034,6 +6068,12 @@ int spi_nor_scan(struct spi_nor *nor)
 	nor->write_reg = spi_nor_write_reg;
 
 	nor->setup = spi_nor_default_setup;
+
+	if (CONFIG_IS_ENABLED(DM_GPIO)) {
+		ret = spi_nor_hw_reset(nor);
+		if (ret)
+			return ret;
+	}
 
 #ifdef CONFIG_SPI_FLASH_SOFT_RESET_ON_BOOT
 	/*

commit e9eb02dd668e7c97f9762aad4aa1023a5017f7b8

mtd: spi-nor: Add kernel-doc for flash_gpio_reset in struct spi_nor
Running ./scripts/kernel-doc -man -v 1>/dev/null over include/linux/mtd/spi-nor.h
revealed the following warning:

include/linux/mtd/spi-nor.h:648: warning: Function parameter or member 'flash_gpio_reset' not described in 'spi_nor'

Add the missing kernel-doc description for the flash_gpio_reset member
of struct spi_nor.

Fixes: 7f77ed0916e0 ("mtd: spi-nor: Add support for flash reset")
Signed-off-by: Michal Simek <michal.simek@amd.com>
Message-ID: <122a81a0cbd3fcf91455ed8632426254d4d0e2e5.1772610195.git.michal.simek@amd.com>

include/linux/mtd/spi-nor.h
@@ -575,6 +575,7 @@ struct spi_flash {
  * @quad_enable:	[FLASH-SPECIFIC] enables SPI NOR quad mode
  * @octal_dtr_enable:	[FLASH-SPECIFIC] enables SPI NOR octal DTR mode.
  * @ready:		[FLASH-SPECIFIC] check if the flash is ready
+ * @flash_gpio_reset:	GPIO descriptor for flash hardware reset
  * @dirmap:		pointers to struct spi_mem_dirmap_desc for reads/writes.
  * @priv:		the private data
  */