m1nl/u-boot-xlnx

commit 65791266cb9db416458ec28a31585d1850d7f0d8

sf: Add lock info option to sf command list
Many SPI flashes have OTP bit for TOP or BOTTOM selection in the
status register that can protect selected regions of the SPI NOR.

Take this bit into consideration while locking the region.
The command "sf lockinfo" shows whether the flash is TOP or
BOTTOM protected, based on this info the offset is provided to
the "sf protect lock" command.

Tested on vck190 board:

Versal> sf probe 0 0 0
SF: Detected is25lp01g with page size 512 Bytes, erase size 128 KiB,
total 256 MiB
Versal> sf erase 0 100000
SF: 1048576 bytes @ 0x0 Erased: OK
Versal>sf lockinfo
Flash is BOTTOM protected
Versal> sf protect lock 0 20000
Versal> sf erase 0 20000
ERROR: flash area is locked
Versal> sf protect unlock 0 20000
Versal> sf erase 0 20000
SF: 131072 bytes @ 0x0 Erased: OK

Signed-off-by: Venkatesh Yadav Abbarapu <venkatesh.abbarapu@amd.com>
Link: https://lore.kernel.org/r/20240809093841.1304-1-venkatesh.abbarapu@amd.com
State: waiting

include/linux/mtd/spi-nor.h
@@ -626,6 +626,7 @@ struct spi_nor {
 	int (*quad_enable)(struct spi_nor *nor);
 	int (*octal_dtr_enable)(struct spi_nor *nor);
 	int (*ready)(struct spi_nor *nor);
+	bool (*flash_lock_info)(struct spi_nor *nor);
 
 	struct {
 		struct spi_mem_dirmap_desc *rdesc;

cmd/sf.c
@@ -420,6 +420,30 @@ static int do_spi_protect(int argc, char *const argv[])
 	return ret == 0 ? 0 : 1;
 }
 
+static int do_spi_lock_info(int argc, char *const argv[])
+{
+	int ret;
+
+	if (argc != 1)
+		return CMD_RET_USAGE;
+
+	ret = spi_flash_lock_info(flash);
+	if (ret < 0) {
+		printf("Flash info is not set\n");
+		return CMD_RET_FAILURE;
+	}
+
+	if (ret == BOTTOM_PROTECT) {
+		printf("Flash is BOTTOM protected\n");
+		return CMD_RET_SUCCESS;
+	} else if (ret == TOP_PROTECT) {
+		printf("Flash is TOP protected\n");
+		return CMD_RET_SUCCESS;
+	}
+
+	return CMD_RET_FAILURE;
+}
+
 enum {
 	STAGE_ERASE,
 	STAGE_CHECK,
@@ -614,6 +638,8 @@ static int do_spi_flash(struct cmd_tbl *cmdtp, int flag, int argc,
 		ret = do_spi_flash_erase(argc, argv);
 	else if (IS_ENABLED(CONFIG_SPI_FLASH_LOCK) && strcmp(cmd, "protect") == 0)
 		ret = do_spi_protect(argc, argv);
+	else if (IS_ENABLED(CONFIG_SPI_FLASH_LOCK) && strcmp(cmd, "lockinfo") == 0)
+		ret = do_spi_lock_info(argc, argv);
 	else if (IS_ENABLED(CONFIG_CMD_SF_TEST) && !strcmp(cmd, "test"))
 		ret = do_spi_flash_test(argc, argv);
 	else
@@ -639,7 +665,9 @@ U_BOOT_LONGHELP(sf,
 	"					  or to start of mtd `partition'\n"
 #ifdef CONFIG_SPI_FLASH_LOCK
 	"sf protect lock/unlock sector len	- protect/unprotect 'len' bytes starting\n"
-	"					  at address 'sector'"
+	"					  at address 'sector'\n"
+	"sf lockinfo				- shows whether the flash locking is top or bottom\n"
+	"					  protected\n"
 #endif
 #ifdef CONFIG_CMD_SF_TEST
 	"\nsf test offset len		- run a very basic destructive test"

drivers/mtd/spi/spi-nor-core.c
@@ -5051,6 +5051,21 @@ static int mx_is_unlocked(struct spi_nor *nor, loff_t ofs, uint64_t len)
 
 	return mx_check_lock_status(nor, ofs, len, sr, cr, false);
 }
+
+static bool mx_flash_lock_info(struct spi_nor *nor)
+{
+	int cr;
+
+	cr = mx_read_cr(nor);
+	if (cr < 0)
+		return cr;
+
+	if (cr & CR_TB_MX)
+		return BOTTOM_PROTECT;
+
+	return TOP_PROTECT;
+}
+
 #endif /* CONFIG_SPI_FLASH_MACRONIX */
 
 #if defined(CONFIG_SPI_FLASH_ISSI)
@@ -5387,6 +5402,21 @@ static int issi_flash_unlock(struct spi_nor *nor, loff_t ofs, uint64_t len)
 	}
 	return ret;
 }
+
+static bool issi_flash_lock_info(struct spi_nor *nor)
+{
+	int fr;
+
+	fr = spi_nor_read_fr(nor);
+	if (fr < 0)
+		return fr;
+
+	if (fr & FR_TB)
+		return BOTTOM_PROTECT;
+
+	return TOP_PROTECT;
+}
+
 #endif /* CONFIG_SPI_FLASH_ISSI */
 
 #if defined(CONFIG_SPI_FLASH_GIGADEVICE)
@@ -5688,6 +5718,21 @@ static int giga_flash_unlock(struct spi_nor *nor, loff_t ofs, uint64_t len)
 	}
 	return ret;
 }
+
+static bool giga_flash_lock_info(struct spi_nor *nor)
+{
+	int status;
+
+	status = read_sr(nor);
+	if (status < 0)
+		return status;
+
+	if (status & SR_TB_GIGA)
+		return BOTTOM_PROTECT;
+
+	return TOP_PROTECT;
+}
+
 #endif /* CONFIG_SPI_FLASH_GIGADEVICE */
 
 #if defined(CONFIG_SPI_FLASH_SPANSION)
@@ -6052,6 +6097,21 @@ static int spansion_flash_unlock(struct spi_nor *nor, loff_t ofs, uint64_t len)
 
 	return 0;
 }
+
+static bool spansion_flash_lock_info(struct spi_nor *nor)
+{
+	int cr;
+
+	cr = spansion_read_cr(nor);
+	if (cr < 0)
+		return cr;
+
+	if (cr & CR_TB_SPAN)
+		return BOTTOM_PROTECT;
+
+	return TOP_PROTECT;
+}
+
 #endif /* CONFIG_SPI_FLASH_SPANSION */
 #endif /* CONFIG_SPI_FLASH_LOCK */
 
@@ -6343,6 +6403,7 @@ int spi_nor_scan(struct spi_nor *nor)
 		nor->flash_lock = mx_lock;
 		nor->flash_unlock = mx_unlock;
 		nor->flash_is_unlocked = mx_is_unlocked;
+		nor->flash_lock_info = mx_flash_lock_info;
 	}
 #endif
 
@@ -6351,6 +6412,7 @@ int spi_nor_scan(struct spi_nor *nor)
 		nor->flash_lock = issi_flash_lock;
 		nor->flash_unlock = issi_flash_unlock;
 		nor->flash_is_unlocked = issi_is_unlocked;
+		nor->flash_lock_info = issi_flash_lock_info;
 	}
 #endif
 
@@ -6359,6 +6421,7 @@ int spi_nor_scan(struct spi_nor *nor)
 		nor->flash_lock = giga_flash_lock;
 		nor->flash_unlock = giga_flash_unlock;
 		nor->flash_is_unlocked = giga_is_unlocked;
+		nor->flash_lock_info = giga_flash_lock_info;
 	}
 #endif
 
@@ -6367,6 +6430,7 @@ int spi_nor_scan(struct spi_nor *nor)
 		nor->flash_lock = spansion_flash_lock;
 		nor->flash_unlock = spansion_flash_unlock;
 		nor->flash_is_unlocked = spansion_is_unlocked;
+		nor->flash_lock_info = spansion_flash_lock_info;
 	}
 #endif
 

include/spi.h
@@ -48,6 +48,9 @@ struct spinand_info;
 
 #define SPI_MEM_DEV_MAX_ID_LEN 6
 
+#define	BOTTOM_PROTECT	1
+#define	TOP_PROTECT	0
+
 /**
  * struct dm_spi_bus - SPI bus info
  *

include/spi_flash.h
@@ -236,4 +236,12 @@ static inline int spi_flash_protect(struct spi_flash *flash, u32 ofs, u32 len,
 		return flash->flash_unlock(flash, ofs, len);
 }
 
+static inline int spi_flash_lock_info(struct spi_flash *flash)
+{
+	if (!flash->flash_lock_info)
+		return -EOPNOTSUPP;
+
+	return flash->flash_lock_info(flash);
+}
+
 #endif /* _SPI_FLASH_H_ */

commit 39c3c64728cd6994282039fd826ba4b92ae5f81b

mtd: spi-nor: Add kernel-doc for flash_lock_info in struct spi_nor
Running ./scripts/kernel-doc -man -v 1>/dev/null over include/linux/mtd/spi-nor.h
revealed the following warning:

include/linux/mtd/spi-nor.h:648: warning: Function parameter or member 'flash_lock_info' not described in 'spi_nor'

Add the missing kernel-doc description for the flash_lock_info member
of struct spi_nor.

Fixes: 65791266cb9d ("sf: Add lock info option to sf command list")
Signed-off-by: Michal Simek <michal.simek@amd.com>
Message-ID: <7ce19ed5e4f2dab316f0d1c3b7cf2efd464303d7.1772610195.git.michal.simek@amd.com>

include/linux/mtd/spi-nor.h
@@ -575,6 +575,7 @@ struct spi_flash {
  * @quad_enable:	[FLASH-SPECIFIC] enables SPI NOR quad mode
  * @octal_dtr_enable:	[FLASH-SPECIFIC] enables SPI NOR octal DTR mode.
  * @ready:		[FLASH-SPECIFIC] check if the flash is ready
+ * @flash_lock_info:	[FLASH-SPECIFIC] get flash lock info
  * @flash_gpio_reset:	GPIO descriptor for flash hardware reset
  * @dirmap:		pointers to struct spi_mem_dirmap_desc for reads/writes.
  * @priv:		the private data