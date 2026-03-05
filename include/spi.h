m1nl/u-boot-xlnx

commit be4fd96247f1539e106cb95e8fb8b0a392001eca

spi: cadence_qspi: Modified the rx_dll_tuning function to support generic execution
Switching the OSPI controller from SDR to DDR mode requires Rx DLL tuning,
which entails adjusting the TX delay, incrementing the RX delay, and
confirming correct flash IDs by reading from the flash.

In the existing setup, spi-mem operations are hardcoded for Rx DLL tuning.
However, for certain flash types such as Macronix, an inversion of opcode
is necessary. To accommodate this requirement, spi-mem operations need to
be received from the nor-framework. Consequently, the rx_dll_tuning function
has been modified to enable generic execution.

Moreover, support has been extended to enable DDR mode for Micron flash.

In the spi_nor_micron_octal_dtr_enable function, the cmd->buf is utilized
to read the flash ID during RX DLL tuning.

This patch is a subset of the patch available at the link below.

Link: https://lore.kernel.org/all/fc520c7b6ed4d84e28e2eb807f2e57b1847d3c93.1710098033.git.tejas.arvind.bhumkar@amd.com/
Signed-off-by: Tejas Bhumkar <tejas.arvind.bhumkar@amd.com>
Acked-by: Venkatesh Yadav Abbarapu <venkatesh.abbarapu@amd.com>
State: waiting

include/spi.h
@@ -46,6 +46,8 @@ struct spinand_info;
 /* Max no. of CS supported per spi device */
 #define SPI_CS_CNT_MAX	2
 
+#define SPI_MEM_DEV_MAX_ID_LEN 6
+
 /**
  * struct dm_spi_bus - SPI bus info
  *
@@ -153,6 +155,7 @@ struct spi_slave {
 	unsigned int bus;
 	unsigned int cs;
 #endif
+	u8 device_id[SPI_MEM_DEV_MAX_ID_LEN];
 	uint mode;
 	unsigned int wordlen;
 	unsigned int max_read_size;

drivers/mtd/spi/spi-nor-core.c
@@ -1574,8 +1574,10 @@ static const struct flash_info *spi_nor_read_id(struct spi_nor *nor)
 	info = spi_nor_ids;
 	for (; info->name; info++) {
 		if (info->id_len) {
-			if (!memcmp(info->id, id, info->id_len))
+			if ((!memcmp(info->id, id, info->id_len)) &&
+			    memcpy(nor->spi->device_id, id, SPI_NOR_MAX_ID_LEN)) {
 				return info;
+			}
 		}
 	}
 
@@ -4123,7 +4125,7 @@ static struct spi_nor_fixups s28hx_t_fixups = {
 static int spi_nor_micron_octal_dtr_enable(struct spi_nor *nor)
 {
 	struct spi_mem_op op;
-	u8 buf;
+	u8 *buf = nor->cmd_buf;
 	u8 addr_width = 3;
 	int ret;
 
@@ -4132,12 +4134,12 @@ static int spi_nor_micron_octal_dtr_enable(struct spi_nor *nor)
 	if (ret)
 		return ret;
 
-	buf = 20;
+	*buf = 20;
 	op = (struct spi_mem_op)
 		SPI_MEM_OP(SPI_MEM_OP_CMD(SPINOR_OP_MT_WR_ANY_REG, 1),
 			   SPI_MEM_OP_ADDR(addr_width, SPINOR_REG_MT_CFR1V, 1),
 			   SPI_MEM_OP_NO_DUMMY,
-			   SPI_MEM_OP_DATA_OUT(1, &buf, 1));
+			   SPI_MEM_OP_DATA_OUT(1, buf, 1));
 	ret = spi_mem_exec_op(nor->spi, &op);
 	if (ret)
 		return ret;
@@ -4153,17 +4155,32 @@ static int spi_nor_micron_octal_dtr_enable(struct spi_nor *nor)
 		return ret;
 
 	nor->spi->flags |= SPI_XFER_SET_DDR;
-	buf = SPINOR_MT_OCT_DTR;
+	*buf = SPINOR_MT_OCT_DTR;
 	op = (struct spi_mem_op)
 		SPI_MEM_OP(SPI_MEM_OP_CMD(SPINOR_OP_MT_WR_ANY_REG, 1),
 			   SPI_MEM_OP_ADDR(addr_width, SPINOR_REG_MT_CFR0V, 1),
 			   SPI_MEM_OP_NO_DUMMY,
-			   SPI_MEM_OP_DATA_OUT(1, &buf, 1));
+			   SPI_MEM_OP_DATA_OUT(1, buf, 1));
 	ret = spi_mem_exec_op(nor->spi, &op);
 	if (ret) {
 		dev_err(nor->dev, "Failed to enable octal DTR mode\n");
 		return ret;
 	}
+
+	/* Read flash ID to make sure the switch was successful. */
+	op = (struct spi_mem_op)
+		SPI_MEM_OP(SPI_MEM_OP_CMD(SPINOR_OP_RDID, 0),
+			   SPI_MEM_OP_ADDR(0, 0, 0),
+			   SPI_MEM_OP_DUMMY(8, 0),
+			   SPI_MEM_OP_DATA_IN(SPI_NOR_MAX_ID_LEN, buf, 0));
+
+	spi_nor_setup_op(nor, &op, SNOR_PROTO_8_8_8_DTR);
+
+	ret = spi_mem_exec_op(nor->spi, &op);
+	if (ret)
+		return ret;
+
+	nor->reg_proto = SNOR_PROTO_8_8_8_DTR;
 
 	return 0;
 }
@@ -4177,20 +4194,19 @@ static void mt35xu512aba_late_init(struct spi_nor *nor,
 static void mt35xu512aba_post_sfdp_fixup(struct spi_nor *nor,
 					 struct spi_nor_flash_parameter *params)
 {
-	if (!CONFIG_IS_ENABLED(SPI_FLASH_DTR_ENABLE))
-		return;
+	if (params->hwcaps.mask & SNOR_HWCAPS_READ_8_8_8_DTR) {
+		/* Set the Fast Read settings. */
+		params->hwcaps.mask |= SNOR_HWCAPS_READ_8_8_8_DTR;
+		spi_nor_set_read_settings(&params->reads[SNOR_CMD_READ_8_8_8_DTR],
+					  0, 20, SPINOR_OP_MT_DTR_RD,
+					  SNOR_PROTO_8_8_8_DTR);
 
-	/* Set the Fast Read settings. */
-	params->hwcaps.mask |= SNOR_HWCAPS_READ_8_8_8_DTR;
-	spi_nor_set_read_settings(&params->reads[SNOR_CMD_READ_8_8_8_DTR],
-				  0, 20, SPINOR_OP_MT_DTR_RD,
-				  SNOR_PROTO_8_8_8_DTR);
+		params->hwcaps.mask |= SNOR_HWCAPS_PP_8_8_8_DTR;
 
-	params->hwcaps.mask |= SNOR_HWCAPS_PP_8_8_8_DTR;
-
-	nor->cmd_ext_type = SPI_NOR_EXT_REPEAT;
-	params->rdsr_dummy = 8;
-	params->rdsr_addr_nbytes = 0;
+		nor->cmd_ext_type = SPI_NOR_EXT_REPEAT;
+		params->rdsr_dummy = 8;
+		params->rdsr_addr_nbytes = 0;
+	}
 
 	/*
 	 * SCCR Map 22nd DWORD does not indicate DTR Octal Mode Enable

drivers/spi/cadence_qspi.c
@@ -334,26 +334,25 @@ static int cadence_spi_set_mode(struct udevice *bus, uint mode)
 	return 0;
 }
 
-static int cadence_qspi_rx_dll_tuning(struct cadence_spi_priv *priv,
+static int cadence_qspi_rx_dll_tuning(struct spi_slave *spi, const struct spi_mem_op *op,
 				      u32 txtap, u8 extra_dummy)
 {
+	struct udevice *bus = spi->dev->parent;
+	struct cadence_spi_priv *priv = dev_get_priv(bus);
 	void *regbase = priv->regbase;
 	int ret, i, j;
-	u8 id[CQSPI_READ_ID_LEN + 1], min_rxtap = 0, max_rxtap = 0, avg_rxtap,
+	u8 *id = op->data.buf.in;
+	u8 min_rxtap = 0, max_rxtap = 0, avg_rxtap,
 	max_tap, windowsize, dummy_flag = 0, max_index = 0, min_index = 0;
 	s8 max_windowsize = -1;
+	unsigned int reg;
 	bool id_matched, rxtapfound = false;
-	struct spi_mem_op op =
-		SPI_MEM_OP(SPI_MEM_OP_CMD(CQSPI_READ_ID | (CQSPI_READ_ID << 8), 8),
-			   SPI_MEM_OP_NO_ADDR,
-			   SPI_MEM_OP_DUMMY(8, 8),
-			   SPI_MEM_OP_DATA_IN(CQSPI_READ_ID_LEN, id, 8));
 
-	op.cmd.nbytes = 2;
-	op.dummy.nbytes *= 2;
-	op.cmd.dtr = true;
-	op.addr.dtr = true;
-	op.data.dtr = true;
+	/* Return if octal-spi disabled */
+	reg = readl(regbase + CQSPI_REG_CONFIG);
+	reg &= CQSPI_REG_CONFIG_ENABLE;
+	if (!reg)
+		return 0;
 
 	max_tap = CQSPI_MAX_DLL_TAPS;
 	/*
@@ -383,9 +382,9 @@ static int cadence_qspi_rx_dll_tuning(struct cadence_spi_priv *priv,
 			}
 		}
 
-		ret = cadence_qspi_apb_command_read_setup(priv, &op);
+		ret = cadence_qspi_apb_command_read_setup(priv, op);
 		if (!ret) {
-			ret = cadence_qspi_apb_command_read(priv, &op);
+			ret = cadence_qspi_apb_command_read(priv, op);
 			if (ret < 0) {
 				printf("error %d reading JEDEC ID\n", ret);
 				return ret;
@@ -393,8 +392,8 @@ static int cadence_qspi_rx_dll_tuning(struct cadence_spi_priv *priv,
 		}
 
 		id_matched = true;
-		for (j = 0; j < CQSPI_READ_ID_LEN; j++) {
-			if (priv->device_id[j] != id[j]) {
+		for (j = 0; j < op->data.nbytes; j++) {
+			if (spi->device_id[j] != id[j]) {
 				id_matched = false;
 				break;
 			}
@@ -461,8 +460,9 @@ static int cadence_qspi_rx_dll_tuning(struct cadence_spi_priv *priv,
 	return avg_rxtap;
 }
 
-static int cadence_spi_setdlldelay(struct udevice *bus)
+static int cadence_spi_setdlldelay(struct spi_slave *spi, const struct spi_mem_op *op)
 {
+	struct udevice *bus = spi->dev->parent;
 	struct cadence_spi_priv *priv = dev_get_priv(bus);
 	void *regbase = priv->regbase;
 	u32 txtap;
@@ -513,7 +513,7 @@ static int cadence_spi_setdlldelay(struct udevice *bus)
 		if (extra_dummy)
 			priv->extra_dummy = true;
 
-		rxtap = cadence_qspi_rx_dll_tuning(priv, txtap, extra_dummy);
+		rxtap = cadence_qspi_rx_dll_tuning(spi, op, txtap, extra_dummy);
 		if (extra_dummy && rxtap < 0) {
 			printf("Failed RX dll tuning\n");
 			return rxtap;
@@ -587,8 +587,9 @@ static int priv_setup_ddrmode(struct udevice *bus)
 	return 0;
 }
 
-static int cadence_spi_setup_ddrmode(struct udevice *bus)
+static int cadence_spi_setup_ddrmode(struct spi_slave *spi, const struct spi_mem_op *op)
 {
+	struct udevice *bus = spi->dev->parent;
 	struct cadence_spi_priv *priv = dev_get_priv(bus);
 	int ret;
 
@@ -600,7 +601,7 @@ static int cadence_spi_setup_ddrmode(struct udevice *bus)
 		return ret;
 
 	priv->edge_mode = CQSPI_EDGE_MODE_DDR;
-	ret = cadence_spi_setdlldelay(bus);
+	ret = cadence_spi_setdlldelay(spi, op);
 	if (ret) {
 		printf("DDR tuning failed with error %d\n", ret);
 		return ret;
@@ -665,9 +666,6 @@ static int cadence_spi_mem_exec_op(struct spi_slave *spi,
 			return err;
 	}
 
-	if (!CONFIG_IS_ENABLED(SPI_FLASH_DTR_ENABLE) && op->cmd.dtr)
-		return 0;
-
 	if (spi->flags & SPI_XFER_U_PAGE)
 		priv->cs = CQSPI_CS1;
 	else
@@ -728,9 +726,8 @@ static int cadence_spi_mem_exec_op(struct spi_slave *spi,
 		break;
 	}
 
-	if (CONFIG_IS_ENABLED(SPI_FLASH_DTR_ENABLE) &&
-		(spi->flags & SPI_XFER_SET_DDR))
-		err = cadence_spi_setup_ddrmode(bus);
+	if ((spi->flags & SPI_XFER_SET_DDR) && op->cmd.dtr)
+		err = cadence_spi_setup_ddrmode(spi, op);
 
 	return err;
 }

commit ef987a745d943e8180258324f4e0e12ccf8372cb

spi: Add kernel-doc for device_id in struct spi_slave
Running ./scripts/kernel-doc -man -v 1>/dev/null over include/spi.h
revealed the following warning:

include/spi.h:186: warning: Function parameter or member 'device_id' not described in 'spi_slave'

Add the missing kernel-doc description for the device_id member of
struct spi_slave.

Fixes: be4fd96247f1 ("spi: cadence_qspi: Modified the rx_dll_tuning function to support generic execution")
Signed-off-by: Michal Simek <michal.simek@amd.com>
Message-ID: <0ace1ce18aa489028682c1f92d7b00b6a241dec7.1772610195.git.michal.simek@amd.com>

include/spi.h
@@ -149,6 +149,7 @@ enum spi_polarity {
  *			be written at once.
  * @memory_map:		Address of read-only SPI flash access.
  * @flags:		Indication of SPI flags.
+ * @device_id:		SPI memory device ID bytes
  */
 struct spi_slave {
 #if CONFIG_IS_ENABLED(DM_SPI)