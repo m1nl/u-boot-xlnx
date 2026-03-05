m1nl/u-boot-xlnx

commit 5fe3c0d01052e81e4751f1913355502ef1df28ad

mtd: spi-nor: Store stacked memories sizes in parameter array
Each flash connected in stacked mode should have a array of integers
to the nor structure to store each flash size. This array stores the
sizes parameters of each flash connected in a stacked or parallel mode.

The current stacked implementation assumes a maximum of two flashes
connected in stacked mode, with both flashes being of the same make
but potentially differing in size. Thus, except for the sizes, all
other flash parameters of both flashes are identical.

In the current implementation of stacked mode operation, the full
chip erase in stack mode does not work correctly for the upper flash.
This problem occurs because spi-nor cannot set the SPI_XFER_U_PAGE
flag. It only sets this flag for the lower flash and
calls spi_nor_erase_chip(), which erases only the lower flash.
Also, spi-nor does not know which chip select values to use.
It will select the upper flash if SPI_XFER_U_PAGE is set, otherwise,
it will select the lower flash.

To fix this issue, spi-nor needs to send the full chip erase command
to both the upper and lower flash. Since spi-nor does not know the
chip select values, it will determine which flash to erase based on
the individual flash sizes stored in array of params. The code changes
will loop through the number of flashes and set the SPI_XFER_U_PAGE
flag to indicate the upper flash. Using the individual index,
spi-nor will call spi_nor_erase_chip() for each flash and wait until
the chip is ready.

Signed-off-by: Prasad Kummari <prasad.kummari@amd.com>
Signed-off-by: Venkatesh Yadav Abbarapu <venkatesh.abbarapu@amd.com>
Link: https://lore.kernel.org/u-boot/20241028082723.3842308-1-prasad.kummari@amd.com/
State: waiting

include/linux/mtd/spi-nor.h
@@ -639,6 +639,9 @@ struct spi_nor {
 	u32 size;
 	u32 sector_size;
 	u32 erase_size;
+	/* Number of flashes connected in parallel or stacked mode parameters */
+	u32 num_flash;
+	u64 cs_params[SNOR_FLASH_CNT_MAX];
 };
 
 #ifndef __UBOOT__

drivers/mtd/spi/spi-nor-core.c
@@ -1069,7 +1069,7 @@ static int spi_nor_erase_chip(struct spi_nor *nor)
 	if (ret)
 		return ret;
 
-	return nor->mtd.size;
+	return ret;
 }
 
 /*
@@ -1110,7 +1110,7 @@ static int spi_nor_erase(struct mtd_info *mtd, struct erase_info *instr)
 	struct spi_nor *nor = mtd_to_spi_nor(mtd);
 	u32 addr, len, rem, offset, max_size;
 	bool addr_known = false;
-	int ret, err;
+	int ret, err, idx = 0, rc = 0;
 
 	dev_dbg(nor->dev, "at 0x%llx, len %lld\n", (long long)instr->addr,
 		(long long)instr->len);
@@ -1158,7 +1158,34 @@ static int spi_nor_erase(struct mtd_info *mtd, struct erase_info *instr)
 
 		if (len == mtd->size &&
 		    !(nor->flags & SNOR_F_NO_OP_CHIP_ERASE)) {
-			ret = spi_nor_erase_chip(nor);
+			if (nor->flags & SNOR_F_HAS_STACKED) {
+				ret = 0; idx = 0;
+				while (idx < nor->num_flash) {
+					if (idx == 1) {
+						nor->spi->flags |= SPI_XFER_U_PAGE;
+						rc = write_enable(nor);
+						if (rc < 0)
+							goto erase_err;
+					}
+					rc = spi_nor_erase_chip(nor);
+					if (rc < 0)
+						goto erase_err;
+
+					rc = spi_nor_erase_chip_wait_till_ready(nor,
+										nor->cs_params[idx]);
+					if (rc)
+						goto erase_err;
+
+					ret += nor->cs_params[idx];
+					idx++;
+				}
+
+			} else {
+				ret = spi_nor_erase_chip(nor);
+				if (ret < 0)
+					goto erase_err;
+				ret = nor->mtd.size;
+			}
 		} else {
 			ret = spi_nor_erase_sector(nor, offset);
 		}
@@ -1169,7 +1196,7 @@ static int spi_nor_erase(struct mtd_info *mtd, struct erase_info *instr)
 		len -= ret;
 
 		if (max_size == mtd->size &&
-		    !(nor->flags & SNOR_F_NO_OP_CHIP_ERASE)) {
+		    !(nor->flags & SNOR_F_NO_OP_CHIP_ERASE) && !idx) {
 			ret = spi_nor_erase_chip_wait_till_ready(nor, mtd->size);
 		} else {
 			ret = spi_nor_wait_till_ready(nor);
@@ -3292,6 +3319,7 @@ static int spi_nor_init_params(struct spi_nor *nor,
 		struct udevice *dev = nor->spi->dev;
 		u32 idx = 0, i = 0;
 		int rc;
+		nor->num_flash = 0;
 
 		/*
 		 * The flashes that are connected in stacked mode should be of same make.
@@ -3314,6 +3342,7 @@ static int spi_nor_init_params(struct spi_nor *nor,
 				if (!(nor->spi->flags & SPI_XFER_STACKED))
 					nor->spi->flags |= SPI_XFER_STACKED;
 			}
+			nor->num_flash++;
 		}
 
 		i = 0;
@@ -3331,12 +3360,15 @@ static int spi_nor_init_params(struct spi_nor *nor,
 				if (!(nor->flags & SNOR_F_HAS_PARALLEL))
 					nor->flags |= SNOR_F_HAS_PARALLEL;
 			}
+			nor->num_flash++;
 		}
 
 		if (nor->flags & (SNOR_F_HAS_STACKED | SNOR_F_HAS_PARALLEL)) {
 			params->size = 0;
-			for (idx = 0; idx < SNOR_FLASH_CNT_MAX; idx++)
+			for (idx = 0; idx < nor->num_flash; idx++) {
+				nor->cs_params[idx] = flash_size[idx];
 				params->size += flash_size[idx];
+			}
 		}
 		/*
 		 * In parallel-memories the erase operation is

commit f0fd3aa5ef0ae1eb3ceabbbd0fd2d9e2992a2f45

mtd: spi-nor: Add kernel-doc for num_flash and cs_params in struct spi_nor
Running ./scripts/kernel-doc -man -v 1>/dev/null over include/linux/mtd/spi-nor.h
revealed the following warnings:

include/linux/mtd/spi-nor.h:648: warning: Function parameter or member 'num_flash' not described in 'spi_nor'
include/linux/mtd/spi-nor.h:648: warning: Function parameter or member 'cs_params' not described in 'spi_nor'

Add the missing kernel-doc descriptions for the num_flash and cs_params
members of struct spi_nor.

Fixes: 5fe3c0d01052 ("mtd: spi-nor: Store stacked memories sizes in parameter array")
Signed-off-by: Michal Simek <michal.simek@amd.com>
Message-ID: <15557b910af2d02c0354011ef8d3b333781c400a.1772610195.git.michal.simek@amd.com>

include/linux/mtd/spi-nor.h
@@ -579,6 +579,8 @@ struct spi_flash {
  * @flash_gpio_reset:	GPIO descriptor for flash hardware reset
  * @dirmap:		pointers to struct spi_mem_dirmap_desc for reads/writes.
  * @priv:		the private data
+ * @num_flash:		number of flashes connected in parallel or stacked mode
+ * @cs_params:		chip select parameters for each flash
  */
 struct spi_nor {
 	struct mtd_info		mtd;