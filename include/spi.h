m1nl/u-boot-xlnx

commit d7a200e54f3b242d129d1660ad96569f67406c39

mtd: spi-nor: Add support for cross die read in dual flash configuration
In a dual parallel configuration, halve the read offset.
Determine whether the read offset points to the lower or
upper flash in a dual stacked configuration and set the
corresponding flags accordingly.

Include support for cases where the read involves an odd
number of bytes.

Extend support for cross-die reads in flash memory devices
that contain multiple dies within them.

Signed-off-by: Ashok Reddy Soma <ashok.reddy.soma@xilinx.com>
Signed-off-by: Michal Simek <michal.simek@xilinx.com>
Signed-off-by: Venkatesh Yadav Abbarapu <venkatesh.abbarapu@amd.com>
Link: https://lore.kernel.org/r/025b915f46b4aeffd0a224dae195de36fe68a9f7.1701853668.git.tejas.arvind.bhumkar@amd.com
State: waiting

include/spi.h
@@ -159,7 +159,7 @@ struct spi_slave {
 	unsigned int max_write_size;
 	void *memory_map;
 
-	u8 flags;
+	u32 flags;
 #define SPI_XFER_BEGIN		BIT(0)	/* Assert CS before transfer */
 #define SPI_XFER_END		BIT(1)	/* Deassert CS after transfer */
 #define SPI_XFER_ONCE		(SPI_XFER_BEGIN | SPI_XFER_END)
@@ -175,6 +175,7 @@ struct spi_slave {
 	 */
 	bool multi_cs_cap;
 	u32 bytemode;
+	bool multi_die;		/* flash with multiple dies */
 };
 
 /**

drivers/mtd/spi/spi-nor-core.c
@@ -1587,13 +1587,11 @@ static int spi_nor_read(struct mtd_info *mtd, loff_t from, size_t len,
 			size_t *retlen, u_char *buf)
 {
 	struct spi_nor *nor = mtd_to_spi_nor(mtd);
-	loff_t offset = from;
-	u32 rem_bank_len = 0;
-	u32 stack_shift = 0;
-	size_t read_len;
-	u8 bank;
 	int ret;
+	u32 offset = from;
 	bool is_ofst_odd = false;
+	u32 bank_size, stack_shift = 0, read_len = 0, rem_bank_len = 0;
+	u8 bank, cur_bank, nxt_bank;
 
 	dev_dbg(nor->dev, "from 0x%08x, len %zd\n", (u32)from, len);
 
@@ -1631,6 +1629,40 @@ static int spi_nor_read(struct mtd_info *mtd, loff_t from, size_t len,
 				} else {
 					nor->spi->flags &= ~SPI_XFER_U_PAGE;
 				}
+			}
+		}
+
+		if (nor->addr_width == 4) {
+			/*
+			 * Some flash devices like N25Q512 have multiple dies
+			 * in it. Read operation in these devices is bounded
+			 * by its die segment. In a continuous read, across
+			 * multiple dies, when the last byte of the selected
+			 * die segment is read, the next byte read is the
+			 * first byte of the same die segment. This is Die
+			 * cross over issue. So to handle this issue, split
+			 * a read transaction, that spans across multiple
+			 * banks, into one read per bank. Bank size is 16MB
+			 * for single and dual stacked mode and 32MB for dual
+			 * parallel mode.
+			 */
+			if (nor->spi && nor->spi->multi_die) {
+				bank_size = SZ_16M;
+				if (nor->flags & SNOR_F_HAS_PARALLEL)
+					bank_size <<= 1;
+				cur_bank = offset / bank_size;
+				nxt_bank = (offset + len) / bank_size;
+				if (cur_bank != nxt_bank)
+					rem_bank_len = (bank_size *
+							(cur_bank + 1)) -
+							offset;
+				else
+					rem_bank_len = (mtd->size >>
+							stack_shift) -
+							offset;
+			} else {
+				rem_bank_len = (mtd->size >> stack_shift) -
+						offset;
 			}
 		}
 

commit 6779c1adc42fd051fa17e6190651c44e90cd104a

spi: Add kernel-doc for multi_die in struct spi_slave
Running ./scripts/kernel-doc -man -v 1>/dev/null over include/spi.h
revealed the following warning:

include/spi.h:186: warning: Function parameter or member 'multi_die' not described in 'spi_slave'

Add the missing kernel-doc description for the multi_die member of
struct spi_slave.

Fixes: d7a200e54f3b ("mtd: spi-nor: Add support for cross die read in dual flash configuration")
Signed-off-by: Michal Simek <michal.simek@amd.com>
Message-ID: <fd8517fa4cf88a2315cba0c41446e160b399b09a.1772610195.git.michal.simek@amd.com>

include/spi.h
@@ -150,6 +150,7 @@ enum spi_polarity {
  * @memory_map:		Address of read-only SPI flash access.
  * @flags:		Indication of SPI flags.
  * @device_id:		SPI memory device ID bytes
+ * @multi_die:		flag indicating flash with multiple dies
  */
 struct spi_slave {
 #if CONFIG_IS_ENABLED(DM_SPI)