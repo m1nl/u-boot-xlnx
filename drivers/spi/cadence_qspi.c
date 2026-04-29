m1nl/u-boot-xlnx

commit 081dde1a07556a8a60e673b07b6c5195add4b2c9

spi: cadence_qspi: Add support for ospi dual stacked
Add support for ospi dual stacked flash configuration.
Read "is-stacked" property from dt and export it to spi-nor framework
via slave->option. Based on the address/offset spi-nor framework will
populate CS through flags for read, write and erase functions.
configure chip select in cadence_qspi driver based on the flags
received from spi-nor framework.

Signed-off-by: Ashok Reddy Soma <ashok.reddy.soma@xilinx.com>
State: pending

drivers/spi/cadence_qspi.c
@@ -110,7 +110,7 @@ static int spi_calibration(struct udevice *bus, uint hz)
 
 	/* just to ensure we do once only when speed or chip select change */
 	priv->qspi_calibrated_hz = hz;
-	priv->qspi_calibrated_cs = spi_chip_select(bus);
+	priv->qspi_calibrated_cs = priv->cs;
 
 	return 0;
 }
@@ -133,7 +133,7 @@ static int cadence_spi_set_speed(struct udevice *bus, uint hz)
 	 */
 	if (priv->previous_hz != hz ||
 	    priv->qspi_calibrated_hz != hz ||
-	    priv->qspi_calibrated_cs != spi_chip_select(bus)) {
+	    priv->qspi_calibrated_cs != priv->cs) {
 		err = spi_calibration(bus, hz);
 		if (err)
 			return err;
@@ -153,8 +153,10 @@ static int cadence_spi_set_speed(struct udevice *bus, uint hz)
 static int cadence_spi_child_pre_probe(struct udevice *bus)
 {
 	struct spi_slave *slave = dev_get_parent_priv(bus);
+	struct cadence_spi_priv *priv = dev_get_priv(bus->parent);
 
 	slave->bytemode = SPI_4BYTE_MODE;
+	slave->option = priv->is_dual;
 
 	return 0;
 }
@@ -172,6 +174,7 @@ static int cadence_spi_probe(struct udevice *bus)
 
 	priv->regbase = plat->regbase;
 	priv->ahbbase = plat->ahbbase;
+	priv->is_dual = plat->is_dual;
 
 	ret = reset_get_bulk(bus, &priv->resets);
 	if (ret)
@@ -230,6 +233,13 @@ static int cadence_spi_xfer(struct udevice *dev, unsigned int bitlen,
 		memcpy(cmd_buf, dout, priv->cmd_len);
 	}
 
+	if (flags & SPI_XFER_U_PAGE)
+		priv->cs = CQSPI_CS1;
+	else
+		priv->cs = CQSPI_CS0;
+
+	flags &= ~SPI_XFER_U_PAGE;
+
 	if (flags == (SPI_XFER_BEGIN | SPI_XFER_END)) {
 		/* if start and end bit are set, the data bytes is 0. */
 		data_bytes = 0;
@@ -239,8 +249,7 @@ static int cadence_spi_xfer(struct udevice *dev, unsigned int bitlen,
 	debug("%s: len=%zu [bytes]\n", __func__, data_bytes);
 
 	/* Set Chip select */
-	cadence_qspi_apb_chipselect(base, spi_chip_select(dev),
-				    plat->is_decoded_cs);
+	cadence_qspi_apb_chipselect(base, priv->cs, plat->is_decoded_cs);
 
 	if ((flags & SPI_XFER_END) || (flags == 0)) {
 		if (priv->cmd_len == 0) {
@@ -339,6 +348,11 @@ static int cadence_spi_ofdata_to_platdata(struct udevice *bus)
 	/* Use 500 KHz as a suitable default */
 	plat->max_hz = ofnode_read_u32_default(subnode, "spi-max-frequency",
 					       500000);
+
+	if (dev_read_u32_default(bus, "is-stacked", -1) == 1)
+		plat->is_dual = CQSPI_DUAL_STACKED_FLASH;
+	else
+		plat->is_dual = CQSPI_SINGLE_FLASH;
 
 	/* Read other parameters from DT */
 	plat->page_size = ofnode_read_u32_default(subnode, "page-size", 256);

drivers/spi/cadence_qspi.h
@@ -15,6 +15,12 @@
 #define CQSPI_DECODER_MAX_CS		16
 #define CQSPI_READ_CAPTURE_MAX_DELAY	16
 
+#define CQSPI_SINGLE_FLASH			0
+#define CQSPI_DUAL_STACKED_FLASH		1
+
+#define CQSPI_CS0				0
+#define CQSPI_CS1				1
+
 #define CQSPI_REG_POLL_US                       1 /* 1us */
 #define CQSPI_REG_RETRY                         10000
 #define CQSPI_POLL_IDLE_RETRY                   3
@@ -202,6 +208,7 @@ struct cadence_spi_platdata {
 	u32		tslch_ns;
 	bool		is_dma;
 	bool		stg_pgm;
+	int		is_dual;
 };
 
 struct cadence_spi_priv {
@@ -211,7 +218,9 @@ struct cadence_spi_priv {
 	u8		cmd_buf[32];
 	size_t		data_len;
 
+	int		is_dual;
 	int		qspi_is_init;
+	unsigned int    cs;
 	unsigned int	qspi_calibrated_hz;
 	unsigned int	qspi_calibrated_cs;
 	unsigned int	previous_hz;

commit e2fca4e6d1bfc57bd54bad3b0f8941283b8d2c02

spi: fix chip select for part-concat flash configs
Both the Cadence OSPI and ZynqMP GQSPI drivers hardcode CS0 as the
default chip select when SPI_XFER_U_PAGE is not set. This works for
stacked-memories configurations where spi-nor-core.c toggles the
SPI_XFER_U_PAGE flag based on address offset, but breaks part-concat
configurations where two independent flash nodes (flash@0 on CS0,
flash@1 on CS1) are concatenated at the MTD layer.

With part-concat, SPI_XFER_U_PAGE is never set because
SNOR_F_HAS_STACKED is not active (no stacked-memories DT property).
As a result, the second flash on CS1 is always accessed on CS0,
causing incorrect reads and potential data corruption.

Fix both drivers to use spi_chip_select() from the DM SPI framework
as the fallback when SPI_XFER_U_PAGE is not set. spi_chip_select()
returns plat->cs[0], which is populated from the DT "reg" property:
  - flash@0 (reg=<0>) -> cs=0  -> CS0
  - flash@1 (reg=<1>) -> cs=1  -> CS1

For stacked-memories (reg=<0>,<1>), spi_chip_select() returns 0 (the
first CS), but this path is only reached for lower-half accesses
since SPI_XFER_U_PAGE handles upper-half CS1 switching. Both
configurations now work correctly.

Fixes: 081dde1a0755 ("spi: cadence_qspi: Add support for ospi dual stacked")
Fixes: 217b0a28b6db ("spi: zynqmp_gqspi: Add parallel memories support in GQSPI driver")
Signed-off-by: Padmarao Begari <padmarao.begari@amd.com>
Message-ID: <20260429072713.1374640-2-padmarao.begari@amd.com>

drivers/spi/cadence_qspi.c
@@ -674,7 +674,7 @@ static int cadence_spi_mem_exec_op(struct spi_slave *spi,
 	if (spi->flags & SPI_XFER_U_PAGE)
 		priv->cs = CQSPI_CS1;
 	else
-		priv->cs = CQSPI_CS0;
+		priv->cs = spi_chip_select(spi->dev);
 
 	/* Set Chip select */
 	cadence_qspi_apb_chipselect(base, priv->cs, priv->is_decoded_cs);

drivers/spi/zynqmp_gqspi.c
@@ -860,7 +860,7 @@ static int zynqmp_qspi_exec_op(struct spi_slave *slave,
 	if (slave->flags & SPI_XFER_U_PAGE)
 		priv->u_page = 1;
 	else
-		priv->u_page = 0;
+		priv->u_page = spi_chip_select(slave->dev);
 
 	if ((slave->flags & GQSPI_SELECT_LOWER_CS) &&
 	    (slave->flags & GQSPI_SELECT_UPPER_CS))