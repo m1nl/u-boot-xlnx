u-boot/u-boot

commit e67ffb5aa5ab03a89305f4575ad3142486f9a306

tpm: rng: Add driver model interface for TPM RNG device
The TPM device has a builtin random number generator(RNG)
functionality. Expose the RNG functions of the TPM device to the
driver model so that they can be used by the EFI_RNG_PROTOCOL if the
protocol is installed.

Also change the function arguments and return type of the random
number functions to comply with the driver model api.

Signed-off-by: Sughosh Ganu <sughosh.ganu@linaro.org>
Reviewed-by: Ilias Apalodimas <ilias.apalodimas@linaro.org>
Signed-off-by: Ilias Apalodimas <ilias.apalodimas@linaro.org>

drivers/rng/tpm_rng.c
@@ -1,0 +1,23 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (c) 2022, Linaro Limited
+ */
+
+#include <dm.h>
+#include <rng.h>
+#include <tpm_api.h>
+
+static int rng_tpm_random_read(struct udevice *dev, void *data, size_t count)
+{
+	return tpm_get_random(dev_get_parent(dev), data, count);
+}
+
+static const struct dm_rng_ops tpm_rng_ops = {
+	.read = rng_tpm_random_read,
+};
+
+U_BOOT_DRIVER(tpm_rng) = {
+	.name	= "tpm-rng",
+	.id	= UCLASS_RNG,
+	.ops	= &tpm_rng_ops,
+};

drivers/rng/Kconfig
@@ -74,4 +74,13 @@ config RNG_SMCCC_TRNG
 	  Enable random number generator for platforms that support Arm
 	  SMCCC TRNG interface.
 
+config TPM_RNG
+	bool "Enable random number generator on TPM device"
+	depends on TPM
+	default y
+	help
+	  The TPM device has an inbuilt random number generator
+	  functionality. Enable random number generator on TPM
+	  devices.
+
 endif

drivers/rng/Makefile
@@ -13,3 +13,4 @@ obj-$(CONFIG_RNG_STM32MP1) += stm32mp1_rng.o
 obj-$(CONFIG_RNG_ROCKCHIP) += rockchip_rng.o
 obj-$(CONFIG_RNG_IPROC200) += iproc_rng200.o
 obj-$(CONFIG_RNG_SMCCC_TRNG) += smccc_trng.o
+obj-$(CONFIG_TPM_RNG) += tpm_rng.o

lib/Kconfig
@@ -360,6 +360,7 @@ source lib/crypt/Kconfig
 config TPM
 	bool "Trusted Platform Module (TPM) Support"
 	depends on DM
+	imply DM_RNG
 	help
 	  This enables support for TPMs which can be used to provide security
 	  features for your board. The TPM can be connected via LPC or I2C

lib/tpm_api.c
@@ -269,7 +269,7 @@ u32 tpm_get_random(struct udevice *dev, void *data, u32 count)
 	if (tpm_is_v1(dev))
 		return tpm1_get_random(dev, data, count);
 	else if (tpm_is_v2(dev))
-		return -ENOSYS; /* not implemented yet */
-	else
-		return -ENOSYS;
+		return tpm2_get_random(dev, data, count);
+
+	return -ENOSYS;
 }

m1nl/u-boot-xlnx

commit f66132b3098e2c278d37c07ce29b1a8a8be287a7

rng: tpm_rng: start TPM before reading random numbers
The tpm-rng driver calls tpm_get_random() without first ensuring the
TPM device is opened and started. When systemd-boot or fdt_fixup_kaslr
triggers an RNG read early in boot, the TPM2 device hasn't been
initialized yet, causing the command to fail:

  tpm-rng tpm-rng: dm_rng_read failed: -1

Fix this by calling tpm_auto_start() on the parent TPM device before
tpm_get_random(). This ensures the TPM is initialized before use.

Fixes: e67ffb5aa5ab ("tpm: rng: Add driver model interface for TPM RNG device")
Signed-off-by: Padmarao Begari <padmarao.begari@amd.com>
Message-ID: <20260429173237.1624398-1-padmarao.begari@amd.com>

drivers/rng/tpm_rng.c
@@ -4,12 +4,20 @@
  */
 
 #include <dm.h>
+#include <log.h>
 #include <rng.h>
 #include <tpm_api.h>
 
 static int rng_tpm_random_read(struct udevice *dev, void *data, size_t count)
 {
-	return tpm_get_random(dev_get_parent(dev), data, count);
+	struct udevice *tpm_dev = dev_get_parent(dev);
+	u32 rc;
+
+	rc = tpm_auto_start(tpm_dev);
+	if (rc)
+		return log_msg_ret("start", -EIO);
+
+	return tpm_get_random(tpm_dev, data, count);
 }
 
 static const struct dm_rng_ops tpm_rng_ops = {